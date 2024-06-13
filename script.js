571c6aa0224e21b1aa3af96add384112f4bacab6
Ankita Sood <ankisood@amazon.com>
2024-06-13 23:31:10 +0530

    Improvement in JS coding and adding a reset button which sets value to 0

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 let value = 0;
 document.querySelector(".score").textContent = value;
+const currentScore = document.querySelector(".score");
 
-let increaseButton = function () {
-  value++;
-  document.querySelector(".score").value = value;
-  document.querySelector(".score").textContent = value;
+const updateScore = function (value) {
+  currentScore.value = value;
+  currentScore.textContent = value;
 };
 
-let decreaseButton = function () {
-  value--;
-  document.querySelector(".score").value = value;
-  document.querySelector(".score").textContent = value;
+const increaseButton = document.querySelector(".increase");
+increaseButton.addEventListener("click", function () {
+  value++;
+  updateScore(value);
+});
+
+const decreaseButton = document.querySelector(".decrease");
+decreaseButton.addEventListener("click", function () {
+  if (value === 1 || value === 0) {
+    alert("Value cannot be further decreased");
+  } else {
+    value--;
+    updateScore(value);
+  }
+});
+
+const reset = function () {
+  value = 0;
+  updateScore(value);
 };
